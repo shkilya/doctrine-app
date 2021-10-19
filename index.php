a0c0e37f8fc553de6b2da570b9d31f68846e1594
shkilniuky <yevhenii.shkilniuk@btcbit.net>
2021-10-19 16:31:29 +0300

    remove comment

--- index.php
+++ index.php
@@ -8,17 +8,14 @@ use Doctrine\ORM\EntityManager;
 /** @var EntityManager $entityManager */
 $entityManager = require __DIR__.'/entityManager.php';
 
-
 $task =  (new Task)
     ->setTitle('cxz')
     ->setAuthor('gfd');
 
-//$entityManager->persist($task);
 $repository = $entityManager->getRepository(Task::class);
 
 var_dump($repository->findAll());
 die;
-//$entityManager->flush();
 
 
 
